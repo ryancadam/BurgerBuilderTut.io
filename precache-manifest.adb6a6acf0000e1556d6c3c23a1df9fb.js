f7ad22059ecaaa5d6c1e59be07203353deaddc9c
Ryan Adam <ryancadam88@gmail.com>
2020-08-26 17:00:19 -0500

    Updates

---
+++ precache-manifest.adb6a6acf0000e1556d6c3c23a1df9fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0887efb44af8458aaf5e7bf00f59c633",
+    "url": "/BurgerBuilderTut.io/index.html"
+  },
+  {
+    "revision": "8dd57ad0864043bbfb83",
+    "url": "/BurgerBuilderTut.io/static/css/main.71764855.chunk.css"
+  },
+  {
+    "revision": "8af1fd47c8b73618758b",
+    "url": "/BurgerBuilderTut.io/static/js/2.bd77d3f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BurgerBuilderTut.io/static/js/2.bd77d3f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd57ad0864043bbfb83",
+    "url": "/BurgerBuilderTut.io/static/js/main.805351bb.chunk.js"
+  },
+  {
+    "revision": "21a08380d82bd3b4772d",
+    "url": "/BurgerBuilderTut.io/static/js/runtime-main.19ea993c.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilderTut.io/static/media/burgerlogo.b8503d26.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ryancadam/BurgerBuilderTut.io.git/precache-manifest.bfa2facb6654d17aece6b13179a780d2.js"
+  "/BurgerBuilderTut.io/precache-manifest.adb6a6acf0000e1556d6c3c23a1df9fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ryancadam/BurgerBuilderTut.io.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BurgerBuilderTut.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
